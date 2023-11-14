7dd2f04dc5c64c4d6c251bd04ec84e522ebbb14d
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-14 09:18:16 -0700

    Create Page for all Home Page components

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import NavBar from '/src/NavBar/NavBar.jsx';
+import Header from '/src/Header/Header.jsx';
+import RecipeSearch from '/src/RecipeSearch/RecipeSearch.jsx';
+
+function Home() {
+  let headerContent = "Lunch and Learn"
+  return (
+    <div>
+      <NavBar />
+      <Header>{headerContent}</Header>
+      <div>
+        <RecipeSearch />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
