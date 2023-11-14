bc05ed60179eb28cdcf13fd79cf1ce154ddd994c
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-14 09:15:50 -0700

    Update RecipeSearch button with routing to RecipesPage

--- src/RecipeSearch/RecipeSearch.jsx
+++ src/RecipeSearch/RecipeSearch.jsx
@@ -1,12 +1,19 @@
 import "./RecipeSearch.css"
+import { useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 
 function RecipeSearch() {
+  let navigate = useNavigate()
+  const routeChange = (path) => {
+      navigate(path)
+  };
+
   return (
     <div className="searchBox">
       <h4>Find Recipes By Country</h4>
       <div>
         <input className="search-bar" type="search" name="q"></input>
-        <button className="search-button">Find Recipes</button>
+        <button className="search-button" onClick={() => routeChange('/recipes')}>Find Recipes</button>
       </div>
       <button className="search-random">Choose Country For me!</button>
     </div>
