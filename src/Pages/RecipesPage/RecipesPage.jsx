189b7be7f3754eda2e82a71ea3463bbc068512f1
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-15 15:13:27 -0700

    Add functionality to support making API call for recipe search

--- src/Pages/RecipesPage/RecipesPage.jsx
+++ src/Pages/RecipesPage/RecipesPage.jsx
@@ -1,17 +1,54 @@
+import './RecipePage.css'
 import NavBar from '/src/NavBar/NavBar.jsx'
 import Header from '/src/Header/Header.jsx'
+import RecipeBox from '/src/RecipeBox/RecipeBox.jsx'
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
-function RecipesPage({children}) {
-  let recipesFound = ["Money", "Cash", "Bread"]
+function RecipesPage() {
+  const location = useLocation();
+  const [recipesFound, setRecipesFound] = useState()
+  let country = location.state
 
-  
+  const createRecipe = (data) => {
+    const Recipe = {
+      country: data.country,
+      imageUrl: data.image_url,
+      title: data.title,
+      url: data.url,
+    }
+    return Recipe;
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/v1/recipes?country=${location.state}`)
+      .then(response => response.json())
+      .then(data => {
+        let recipeObjects = data.data.map((element) => {
+          return createRecipe(element.attributes)
+        });
+        setRecipesFound(recipeObjects)
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  console.log(recipesFound)
+
 
   return (
     <>
-      <NavBar />
-      <Header>{children} Recipes</Header>
-      <button className='button-country'>Learn about {children}</button>
-
+      {recipesFound && 
+        <>
+          <Header>{recipesFound[0].country} Recipes</Header>
+          <button className='button-country'>Learn about {recipesFound[0].country}</button>
+          <div className='recipe-boxes'>
+            {recipesFound.map((recipe) => (
+                <RecipeBox {...recipe} />
+            ))
+            }
+          </div>
+        </>  
+      }
     </>
   );
 }
