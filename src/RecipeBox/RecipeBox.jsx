86ad687ef78243d36210c7a35599d3a9cf92b828
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-15 17:08:12 -0700

    Refactor RecipeBox component to display images with Heart

--- src/RecipeBox/RecipeBox.jsx
+++ src/RecipeBox/RecipeBox.jsx
@@ -1,10 +1,13 @@
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 import './RecipeBox.css'
 
 function RecipeBox(props) {
   return (
-    <>
-      <p className='recipe-box'>{props.title}</p>
-    </>
+    <div className='recipe-box-container'>
+      <img className='recipe-img' src={props.imageUrl} />
+      <div className="favorite-icon"><FaRegHeart /></div>
+    </div>
+    // <img className='recipe-box' src={props.imageUrl} />
   );
 }
 
