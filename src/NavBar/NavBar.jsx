91379bf31d1688a13072e206eb102a183ca75e21
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-15 17:07:25 -0700

    Add React Links to NavBar

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import "./NavBar.css"
 
 function NavBar() {
   return (
     <ul className="navList">
-      <li className="navItem"><a>Home</a></li>
+      <Link className="navItem" to='/'>Home</Link>
       <li className="navItem">|</li>
-      <li className="navItem"><a>Register</a></li>
+      <Link className="navItem">Register</Link>
       <li className="navItem">|</li>
-      <li className="navItem"><a>Log In</a></li>
+      <Link className="navItem">Log In</Link>
     </ul>
   );
 }
